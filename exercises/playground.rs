327c1a18babef17495ff3547f2862f5bfa23e317
Éric NICOLAS <ccjmne@gmail.com>
2024-11-30 23:16:37 +0100

    Push current state of playground

--- exercises/playground.rs
+++ exercises/playground.rs
@@ -1,4 +1,4 @@
-fn borrowing() {
+fn ownership() {
     let s = "Hello";
     {
         let mut s = String::from(s);
@@ -17,8 +17,53 @@ fn borrowing() {
     // --------------------------------------------------------------------------------------------
     let ss = s;
     println!("{s}, {ss}");
+    // --------------------------------------------------------------------------------------------
+    let mut s3 = String::from("Hello");
+    println!("{s3}");
+    s3 = asdf(s3);
+    println!("{s3}");
+    fn asdf(var: String) -> String {
+        var
+    }
+}
+
+fn borrowing() {
+    let s1 = String::from("Hello");
+    let len = calculate_length(&s1);
+    println!("The length of \"{s1}\" is {len}.");
+    fn calculate_length(s: &String) -> usize {
+        s.len()
+    }
+
+    // --------------------------------------------------------------------------------------------
+    let mut s = String::from("Hello");
+    change(&mut s);
+    fn change(s: &mut String) {
+        s.push_str(", world");
+    }
+    println!("{s}");
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{r2}");
+    s.push_str("world");
+    let r3 = &mut s;
+    r3.push_str("world");
+    println!("{r3}");
+    // --------------------------------------------------------------------------------------------
+    // fn dangle() -> &String {
+    //     &String::from("Hello")
+    // }
+    fn no_dangle() -> String {
+        String::from("Hello")
+    }
 }
 
 fn main() {
-    borrowing()
+    println!("OWNERSHIP ---------------------------------------");
+    ownership();
+    println!("BORROWING ---------------------------------------");
+    borrowing();
 }
